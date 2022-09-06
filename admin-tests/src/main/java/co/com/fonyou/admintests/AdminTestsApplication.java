e18ba0ea7d654fd92b3c1824c1e2103bfb606d2f
bonillasan <sbonilla0197@gmail.com>
2022-09-06 14:54:05 -0500

    initial commit

---
+++ admin-tests/src/main/java/co/com/fonyou/admintests/AdminTestsApplication.java
@@ -0,0 +1,13 @@
+package co.com.fonyou.admintests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminTestsApplication.class, args);
+	}
+
+}
---
+++ admin-tests/src/test/java/co/com/fonyou/admintests/AdminTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.fonyou.admintests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
